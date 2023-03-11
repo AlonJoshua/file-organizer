aa34e7cbb3ff371b6c3c7c92445cdb1970c7a936
AlonJoshua <alonjoshua@gmail.com>
2023-03-11 22:48:11 +0200

    No dev tools on start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const fs = require('fs')
-const fse = require('fs-extra')
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -23,7 +21,7 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
